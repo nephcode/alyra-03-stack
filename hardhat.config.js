619b48029cf61ee6ac6126a2bdcfc296760937b3
Neph <nephcohen@gmail.com>
2024-12-30 22:57:57 +0100

    Debug deploy
    j'ai un bug de deploiement que je ne comprends pas du tout

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_URL = process.env.SEPOLIA_URL || ""
+const SEPOLIA_API_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || ""
+const ETHERSCAN_API_PRIVATE_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
-  solidity: "0.8.27",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [`0x${SEPOLIA_API_PRIVATE_KEY}`],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
