fe6fc8a31612d09dfc3dd03dbb216013b2fb923f
Neph <nephcohen@gmail.com>
2025-01-01 05:59:52 +0100

    hardHat classique deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv/config");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
-const SEPOLIA_URL = process.env.SEPOLIA_URL || ""
-const SEPOLIA_API_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || ""
-const ETHERSCAN_API_PRIVATE_KEY = process.env.ETHERSCAN_API_KEY || ""
+const SEPOLIA_URL = process.env.SEPOLIA_URL || "";
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY || "";
+const ETHERSCAN_PRIVATE_KEY = process.env.ETHERSCAN_PRIVATE_KEY || "";
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
       url: SEPOLIA_URL,
-      accounts: [`0x${SEPOLIA_API_PRIVATE_KEY}`],
       chainId: 11155111,
+      accounts: [`0x${ETH_PRIVATE_KEY}`]
     },
     localhost: {
       url: "http://127.0.0.1:8545",
@@ -19,12 +21,15 @@ module.exports = {
     }
   },
   solidity: {
-    version: "0.8.4",
+    version: "0.8.28",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200
       }
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_PRIVATE_KEY
   }
 };
